9c05cd348e9ffbcdcd59359053c5e53a27299b06
Milen Ivanov <milensl92@gmail.com>
2021-10-17 02:07:31 +0300

    Added Privacy Policy button and page. Index-page changed heading

---
+++ HTML-Previewer-Web_App/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿namespace HTML_Previewer_Web_App.Controllers
+{
+    using HTML_Previewer_Web_App.Models;
+    using HTML_Previewer_Web_App.Models.Samples;
+    using HTML_Previewer_Web_App.Services.Samples;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Diagnostics;
+
+    public class HomeController : Controller
+    {
+        private readonly ISamplesService samples;
+
+        public HomeController(ISamplesService samples)
+            => this.samples = samples;
+
+        public IActionResult Index(string id)
+        {
+            var sample = this.samples.SampleCode(id);
+
+            if (sample != null)
+            {
+                return View(new SampleFormModel { Id = id, Code = sample.Code });
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {            
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HTML-Previewer-Web_App/DataWebConstants.cs
@@ -0,0 +1,21 @@
+﻿namespace HTML_Previewer_Web_App
+{
+    public class DataWebConstants
+    {
+        public const string SystemName = "HTML Previewer Web App";
+
+        public const string SuccessMessageKey = "SuccessMessageKey";
+        public const string ErrorMessageKey = "ErrorMessageKey";
+
+        public const string SuccessfulSavedSample = "Your sample-code was saved successfully.";
+        public const string SuccessfulEditSample = "Your sample-code was edit successfully.";
+
+        public const string InvalidSampleContent = "Your sample-code cannot be empty.";
+        public const string InvalidSampleSize = "Your sample-code cannot be more than 5MB";
+
+        public const int PasswordMinLength = 6;
+        public const int PasswordMaxLength = 100;
+
+        public const int IdMaxLength = 40;
+    }
+}
